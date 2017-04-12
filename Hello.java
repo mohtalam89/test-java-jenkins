98833f3a6057c87ef5e1a20a857c2c9a40ab07b3
mohtalam89 <mohtalam89@gmail.com>
2017-04-12 13:32:38 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
-public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello, World");
-  }
+class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
 }
